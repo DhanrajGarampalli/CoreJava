df5cab37afb39411bcd546606b9aacce8101696a
DhanrajGarampalli <dhanrajsoma1@gmail.com>
2025-06-01 15:23:06 +0530

    All Loops programs

---
+++ For_Loop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class For_Loop {
+	
+	public static void main(String[] args) {
+		
+
+		for(int i=1; i<=2; i++)//printing name 2 times.
+		{
+			System.out.println("Bindu");
+		}
+	}
+	
+
+}
---
+++ For_loop2.java
@@ -0,0 +1,37 @@
+package loops;
+
+public class For_loop2 {
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=10; i++)//Printing name 10 times.
+		{
+			System.out.println("Bindu soma");
+		}
+		
+		for(int i=0; i<=10;i++)//Printing 0 to 10.
+		{
+			System.out.println(i);
+		}
+		
+		for(int s=10; s>=0; s--)//Printing 10 to 0.
+		{
+			System.out.println(s);
+		}
+		
+		for (int i=-5; i<=5; i++)//Printing -5 to 5.
+		{
+			System.out.println(i);
+		}
+		
+		/*for(int i=0; i<1; i--)//Printing 0 to -ve.
+		{
+			System.out.println(i);
+		}*/
+		
+		for(int i=0; i>-1; i++)//printing 0 to +ve.
+		{
+			System.out.println(i);
+		}
+	}
+
+}
